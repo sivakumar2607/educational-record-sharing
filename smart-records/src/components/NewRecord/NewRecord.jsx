5524eee772749174896b5b51e5f1032a09e52f2a
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-25 23:53:10 +0530

    ADD.newrecord component is done

--- smart-records/src/components/NewRecord/NewRecord.jsx
+++ smart-records/src/components/NewRecord/NewRecord.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import './NewRecord.css'
 function NewRecord() {
   return (
-    <div>NewRecord</div>
+    <div>
+      <div className="newrecord-container">
+        <div className='newrecord-left'>
+          <div className="newrecord-left-heading">
+            New Record
+          </div>
+          <form className='newrecord-inputs'>
+            <label>
+              Student ID
+            </label>
+            <input type='text' className='newrecord-studentid'/>
+            <label>
+              Institute Name
+            </label>
+            <input type='text' className='newrecord-instituteid'/>
+            <label>
+              Record Name
+            </label>
+            <input type='text' className='newrecord-recordname'/>
+            <div className='newrecord-section-btn'>
+              <button className='newrecord-btn'>Upload to DataBase</button>
+            </div>
+          </form>
+        </div>
+        <div className="newrecord-right">
+          <div className='newrecord-right-heading'>
+            Enter the Description
+          </div>
+          <textarea className='newrecord-right-textarea'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
