d3973a9265de7c77fde00b7b146f17cb40c41a48
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-25 23:25:09 +0530

    ADD.userListOfRequest component is completed

--- smart-records/src/components/UserListOfRequest/UserListOfRequest.jsx
+++ smart-records/src/components/UserListOfRequest/UserListOfRequest.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import './UserListOfRequest.css'
 function UserListOfRequest() {
   return (
-    <div>UserListOfRequest</div>
+    <div>
+     <div className="userlist-heading">
+      List of Record Requests
+     </div>
+     <div className='heading-underline'></div>
+     <div className='userlist-container'>
+      <div className='userlist-section-heading'>
+        <div>Student ID</div>
+        <div>Record Name</div>
+        <div>Description</div>
+        <div>Created Timestamp</div>
+      </div>
+      <div className="userlist-section-content">
+        <div>17IT270</div>
+        <div>Academic certificate</div>
+        <div>Required for scholarship application</div>
+        <div>Date</div>
+      </div>
+      </div>
+      <div className='userlist-section-btn'>
+        <button className='userlist-btn'>Add</button>
+      </div>
+    </div>
   )
 }
 
