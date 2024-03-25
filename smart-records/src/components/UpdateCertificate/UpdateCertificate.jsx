299ef07fe14e845d76bdecbc6c854517dc13cb68
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-25 23:09:23 +0530

    MODIFY.updateCertificate jsx and css file is completed

--- smart-records/src/components/UpdateCertificate/UpdateCertificate.jsx
+++ smart-records/src/components/UpdateCertificate/UpdateCertificate.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import './UpdateCertificate.css'
 function UpdateCertificate() {
   return (
-    <div>UpdateCertificate</div>
+    <div>
+      <div className='upload-container'>
+        <div className='upload-heading'>Upload Education Reports</div>
+        <div className='heading-underline'></div>
+        <form className="myForm">
+          <label>INSTITUTE ID</label>
+          <input type='text' className='upload-institute-id'/>
+          <label>STUDENT ID</label>
+          <input type='text' className='upload-student-id' />
+          <label>RECORD NAME</label>
+          <input type='text' className='upload-record-name' />
+          <label>DESCRIPTION</label>
+          <input type='text' className='upload-description' />
+          <label>DOCUMENTS (UPLOAD IN .ZIP OR .RAR FORMAT)</label>
+          <input type='file' className='upload-documents'/>
+          <label>TIMESTAMP</label>
+          <input type='text' className='upload-timestamp'/>
+        </form>
+        <div className="update-section-btn">
+        <button className='upload-btn'>Upload To Database</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
