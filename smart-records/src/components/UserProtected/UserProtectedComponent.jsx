bd12fc6bcaf0369a774eab1ccc36eadf4a21d238
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-27 17:33:43 +0530

    Add.useEffect is used for the key-cloak client

--- smart-records/src/components/UserProtected/UserProtectedComponent.jsx
+++ smart-records/src/components/UserProtected/UserProtectedComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserProtectedComponent() {
   return (
-    <div>UserProtectedComponent</div>
+    <div>Protected</div>
   )
 }
 
--- smart-records/src/hooks/useAuth.jsx
+++ smart-records/src/hooks/useAuth.jsx
@@ -1,6 +1,14 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
+import {KeyCloak} from 'keycloak-js'
 function useAuth(){
-const[isLogin,setLogin] = useState(false)
+const[isLogin,setLogin] = useState(false);
+useEffect(()=>{
+    const client = new KeyCloak({
+        url:0,
+        realm:0,
+        clientId:0,
+})
+},[])
 return isLogin;
 }
 export default useAuth;
