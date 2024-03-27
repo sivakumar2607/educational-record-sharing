0bb3de90f1eb9518d566726df992062957594957
sai babu <saibabu@sais-Air-3.domain.name>
2024-03-27 19:23:01 +0530

    ADD.connected with the keyCloak

--- smart-records/src/hooks/useAuth.jsx
+++ smart-records/src/hooks/useAuth.jsx
@@ -1,13 +1,14 @@
 import React,{useState,useEffect} from "react";
-import {Keycloak} from 'keycloak-js'
+import Keycloak from 'keycloak-js'
 function useAuth(){
 const[isLogin,setLogin] = useState(false);
 useEffect(()=>{
     const client = new Keycloak({
-        url:process.env.VITE_KEYCLOAK_URL,
-        realm:process.env.VITE_KEYCLOAK_REALM,
-        clientId:process.env.VITE_KEYCLOAK_CLIENT,
-})
+        url:import.meta.env.VITE_KEYCLOAK_URL,
+        realm:import.meta.env.VITE_KEYCLOAK_REALM,
+        clientId:import.meta.env.VITE_KEYCLOAK_CLIENT,
+});
+client.init({onLoad:"login-required"}).then((res)=>setLogin(res))
 },[])
 return isLogin;
 }
