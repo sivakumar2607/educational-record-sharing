0cc943334df771c550c3e6f7e9624d839e447b36
sai babu <saibabu@sais-Air.domain.name>
2024-03-31 21:57:37 +0530

    add.functionality for UserLoginPageComponent

--- smart-records/src/components/UserLoginPage/UserLoginPage.jsx
+++ smart-records/src/components/UserLoginPage/UserLoginPage.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import './UserLoginPage.css'
+import React, { useState } from 'react';
+import './UserLoginPage.css';
+import CertificatePage from '../CertificatePage/CertificatePage';
+
 function UserLoginPage() {
+  const [showCertificatePage, setShowCertificatePage] = useState(false);
+
+  const handleNewRequestClick = () => {
+    setShowCertificatePage(false);
+  };
+
+  const handleDownloadClick = () => {
+    setShowCertificatePage(true);
+  };
+
   return (
     <div>
-         <nav className='userLogin-navbar'>
-                <div>New Request</div>
-                <div>Download</div>
-            </nav>
+      <nav className='userLogin-navbar'>
+        <div onClick={handleNewRequestClick}>New Request</div>
+        <div onClick={handleDownloadClick}>Download</div>
+      </nav>
+      {!showCertificatePage && (
         <div className='userLogin-container'>
-            <div className='userLogin-section'>
+          <div className='userLogin-section'>
             <div className='userLogin-btn'><button>+</button></div>
             <div><div className='userLogin-btn-desc'>Create a New Request</div></div>
-            </div>
+          </div>
         </div>
+      )}
+      {showCertificatePage && <CertificatePage />}
     </div>
-  )
+  );
 }
 
-export default UserLoginPage
+export default UserLoginPage;
